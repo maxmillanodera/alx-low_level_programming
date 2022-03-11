5887c904dfbbb737ff67e7d9275f3bb12a3b9839
maxmillanodera <ojwangmaxmillan@gmail.com>
2022-03-11 04:36:38 -0800

    putin

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - entrypoint
+ * main - entry point
  *
  * Return: 0 after printing the function
  */
 int main(void)
 {
 puts("\"programming is like building a multilingual puzzle");
-return (0);
+return (0)
 }
