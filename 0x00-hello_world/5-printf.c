87b39a1378f53ae368210628201b3d9dff516e5a
maxmillanodera <ojwangmaxmillan@gmail.com>
2022-03-11 04:49:06 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point 
+ * My hello, print task
+ * Return: return 1 after program execution 
+ */
+int main(void)
+{
+printf("with proper grammer, but the outcomr is a piece of art,\n");
+return (0);
+}
