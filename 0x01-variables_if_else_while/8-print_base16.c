4efa838802a15d9daa6583ee7d3497e7c5b63e92
maxmillanodera <ojwangmaxmillan@gmail.com>
2022-03-12 07:17:46 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,14 +9,14 @@
 int main(void)
 
 {
-char base_16
+char base_16;
 
 for (base_16 = 48; base_16 <= 57; base_16++)
 {
 putchar(base_16);
 }
 
-for (base_16 = 97 ; base16 <= 102; base_16++)
+for (base_16 = 97 ; base_16 <= 102; base_16++)
 {
 putchar (base_16);
 }
