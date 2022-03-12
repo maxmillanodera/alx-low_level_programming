9cb3f5b98ed51e7b004f11adf75c93e6df689892
maxmillanodera <ojwangmaxmillan@gmail.com>
2022-03-12 05:53:56 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success/correct)
+ */
+
+int main(void)
+
+{
+char alphabet;
+
+for (alphabrt = 'a'; alphabet <= 'z'; alpabet++)
+{
+if (alphabet == 'q')
+continue;
+
+else if (alphabet == 'e')
+continue;
+
+putchar(alphabet);
+}
+
+putchar ('\n');
+
+return (0);
+
+}
