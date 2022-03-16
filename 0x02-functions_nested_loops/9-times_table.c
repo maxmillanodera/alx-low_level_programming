82114d73e6b8bfd7e9892f6b216892d9f3426597
maxmillanodera <ojwangmaxmillan@gmail.com>
2022-03-16 08:08:00 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,47 @@
+#inclide "main.h"
+/**
+ * times_table -prints tables
+ *
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+int a;
+int b;
+int c;
+for (a = 0; a <= 9; a++)
+{
+for (b = 0; b <= 9; b++)
+{
+c = a * c;
+if ((c / 10) == 0)
+{
+if (b == 0)
+{
+_putchar ('0');
+}
+if (b != 0)
+{
+_putchar (' ');
+_putchar ((c % 10) + '0');
+}
+if (b < 9)
+{
+_pitchar(',');
+_putchar(' ');
+}
+}
+else
+{
+_putchar ((c / 10) + '0');
+_putchar ((c % 10) + ')');
+if (b < 9)
+{
+_pitchar(',');
+_putchar (' ');
+}
+}
+}
+_putchar ('\n');
+}
+}
