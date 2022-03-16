cbcbc2596d288b4079a3d797658a1dad64510d26
maxmillanodera <ojwangmaxmillan@gmail.com>
2022-03-16 07:51:08 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -19,7 +19,7 @@ for (b = 0; b <= 59; b++)
 _putchar (a / 10 + '0');
 _putchar (a % 10 + '0');
 _putchar (':');
-_putchar (b / 10 +'0');
+_putchar (b / 10 + '0');
 _putchar (b % 10 + '0');
 _putchar ('\n');
 
