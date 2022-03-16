acdae286d7ded165ae85288a50b6badaf0f7f1dc
maxmillanodera <ojwangmaxmillan@gmail.com>
2022-03-16 06:04:28 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(10)
+{
+int alphabet;
+int count;
+
+
+count = 0;
+while (count < 10)
+{
+_putchar(alphabet);
+}
+
+count++;
+_putchar('\n');
+}
+
+
+}
